4997ca8ae81906e81224f4ba8e8f7f24e9fee5f3
alek <wait82985@gmail.com>
2023-07-06 12:42:59 +0600

    initial commit. 3 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    public void minSubArrayLenTest1() {
+        int target = 7;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int expected = 2;
+        int actual = new Solution().minSubArrayLen(target, nums);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSubArrayLenTest2() {
+        int target = 4;
+        int[] nums = {1, 4, 4};
+        int expected = 1;
+        int actual = new Solution().minSubArrayLen(target, nums);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSubArrayLenTest3() {
+        int target = 11;
+        int[] nums = {1, 1, 1, 1, 1, 1, 1, 1};
+        int expected = 0;
+        int actual = new Solution().minSubArrayLen(target, nums);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
